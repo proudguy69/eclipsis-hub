858257717020f04a3ae9eab532c2335fd350736a
Oliver <jokejupa@gmail.com>
2026-04-01 12:59:13 -0400

    prepare for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,30 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
-  modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/image', 'nuxt-studio'],
+
+  modules: ["nitro-cloudflare-dev", '@nuxt/ui', '@nuxt/content', '@nuxt/image', 'nuxt-studio'],
+
+  studio: {
+    repository: {
+      provider: 'github', // 'github' or 'gitlab'
+      owner: 'proudguy69',
+      repo: 'proudguy69/eclipsis-hib',
+      branch: 'main'
+    }
+  },
+
 
   css: ['~/assets/css/main.css'],
 
   // nitro preset for cloudflare pages
-  // nitro: {
-  //   preset: 'cloudflare-pages',
-  // },
+  nitro: {
+    preset: "cloudflare_module",
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  },
 
   // @nuxt/image config
   image: {
